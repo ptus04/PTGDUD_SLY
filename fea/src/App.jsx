bfbda356f81c173c0998981a1d769fb14a6aed63
Lê Tấn Phong <141659740+LeTanPhong3003@users.noreply.github.com>
2025-04-10 21:40:05 +0700

    Sidebar + Header

--- fea/src/App.jsx
+++ fea/src/App.jsx
@@ -1,11 +1,41 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import "./index.css";
+import Sidebar from "./components/sidebar";
+import Header from "./components/header";
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <Router>
+      <div className="flex h-screen">
+        {/* Sidebar */}
+        <Sidebar />
 
-  return <></>;
+        {/* Main Content */}
+        <div className="flex flex-1 flex-col">
+          {/* Header */}
+          <Header />
+
+          {/* Content */}
+          <main className="flex-1 bg-gray-50 p-6">
+            <Routes>
+              <Route path="/" element={<h1>Dashboard</h1>} />
+              <Route path="/customers" element={<h1>Customers</h1>} />
+              <Route path="/products" element={<h1>Products</h1>} />
+              <Route path="/orders" element={<h1>Orders</h1>} />
+              <Route path="/invoice" element={<h1>Invoice</h1>} />
+              <Route path="/login" element={<h1>Login</h1>} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
