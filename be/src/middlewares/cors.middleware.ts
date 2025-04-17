09b980df758cc6392fdf3b65f9dbd88fff13c593
Phùng Văn Tú <tuphung.03032004@gmail.com>
2025-04-17 14:11:06 +0700

    feat: update CORS middleware to use dynamic origin from config

--- be/src/middlewares/cors.middleware.ts
+++ be/src/middlewares/cors.middleware.ts
@@ -1,7 +1,8 @@
+import config from "../configs/env";
 import { NextFunction, Request, Response } from "express";
 
 const cors = (req: Request, res: Response, next: NextFunction) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", config.corsOrigin);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
