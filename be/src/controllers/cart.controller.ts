6938a7d9df98d250dee2a7581636979e9d7d47f4
Phùng Văn Tú <tuphung.03032004@gmail.com>
2025-04-30 10:15:38 +0700

    refactor: Move matchedData import to the top for better readability

--- be/src/controllers/cart.controller.ts
+++ be/src/controllers/cart.controller.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
+import { matchedData } from "express-validator";
 import CartItem from "../models/CartItem.model";
 import cartService from "../services/cart.service";
-import { matchedData } from "express-validator";
 
 export const getCart = async (req: Request, res: Response) => {
   const userId = req._id;
   const items = await cartService.getCartItems(userId);
   res.status(200).json(items);
 };
+
 export const updateCart = async (req: Request, res: Response) => {
   const userId = req._id;
   const items = matchedData<{ items: CartItem[] }>(req).items;
