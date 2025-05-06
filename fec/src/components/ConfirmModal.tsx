fbbc4fbbec3c7e20944d59c9a96ab92f9fdf69be
Phùng Văn Tú <tuphung.03032004@gmail.com>
2025-05-06 18:36:06 +0700

    feat: Update ConfirmModal to support optional cancel and accept text props

--- fec/src/components/ConfirmModal.tsx
+++ fec/src/components/ConfirmModal.tsx
@@ -6,40 +6,42 @@ interface LogoutConfirmModalProps {
   title: string;
   content: string;
   open: boolean;
+  cancelText?: string;
+  acceptText?: string;
   onClose: () => void;
-  onCancel: () => void;
-  onAccept: () => void;
+  onCancel?: () => void;
+  onAccept?: () => void;
 }
 
 const ConfirmModal = (props: LogoutConfirmModalProps) => {
   return (
     <RenderIf condition={props.open}>
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-        <div className="relative flex max-w-md flex-col gap-4 rounded-md bg-white p-4 shadow-md">
-          {/* Close button */}
+        <div className="relative flex flex-col gap-4 rounded-md bg-white p-4 shadow-md md:min-w-lg">
           <button
             className="absolute top-4 right-4 aspect-square w-6 cursor-pointer text-gray-400 duration-200 hover:bg-red-500 hover:text-white"
             type="button"
             onClick={props.onClose}
-            aria-label="Đóng"
+            title="Đóng"
           >
             <i className="fa fa-close" />
           </button>
 
-          {/* Title */}
-          <h3 className="border-b border-b-gray-300 text-center text-xl font-bold">{props.title}</h3>
+          <h3 className="border-b border-b-gray-300 pb-2 text-center text-xl font-bold">{props.title}</h3>
 
-          {/* Content */}
           <p className="text-center">{props.content}</p>
 
-          {/* Actions */}
           <div className="flex justify-between gap-2">
-            <Button type="button" preset="secondary" onClick={props.onCancel}>
-              Hủy
-            </Button>
-            <Button type="button" preset="primary" onClick={props.onAccept}>
-              Đăng xuất
-            </Button>
+            <RenderIf condition={!!props.onCancel}>
+              <Button type="button" preset="secondary" onClick={props.onCancel}>
+                {props.cancelText ?? "Hủy"}
+              </Button>
+            </RenderIf>
+            <RenderIf condition={!!props.onAccept}>
+              <Button type="button" preset="primary" onClick={props.onAccept}>
+                {props.acceptText ?? "Ok"}
+              </Button>
+            </RenderIf>
           </div>
         </div>
       </div>
