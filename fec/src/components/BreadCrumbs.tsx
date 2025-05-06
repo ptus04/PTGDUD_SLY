01ea29d51453699718384c44a3fb8ad3d7db5297
Phùng Văn Tú <tuphung.03032004@gmail.com>
2025-05-06 17:27:51 +0700

    feat: Add Breadcrumbs component for improved navigation in About Us page

---
+++ fec/src/components/BreadCrumbs.tsx
@@ -0,0 +1,83 @@
+import { ProductWithIdString } from "@be/src/models/Product.model";
+import { memo, useEffect, useState } from "react";
+import { NavLink, useLocation, useSearchParams } from "react-router";
+import RenderIf from "./RenderIf";
+
+type BreadCrumbsProps = {
+  product?: ProductWithIdString;
+};
+
+const Breadcrumbs = (props: BreadCrumbsProps) => {
+  const location = useLocation();
+  const [params] = useSearchParams();
+  const [category, setCategory] = useState(params.get("category"));
+  const [query, setQuery] = useState(params.get("query"));
+
+  useEffect(() => {
+    setCategory(params.get("category"));
+    setQuery(params.get("query"));
+  }, [params]);
+
+  return (
+    <nav className="text-sm text-gray-500">
+      <ol className="flex gap-1">
+        <li>
+          <NavLink className="after:text-gray-500 after:content-['_/_'] hover:text-red-500" to="/">
+            Trang chủ
+          </NavLink>
+        </li>
+
+        <RenderIf condition={/\/products/.test(location.pathname)}>
+          <li>
+            <NavLink className="after:text-gray-500 after:content-['_/_'] hover:text-red-500" to="/products" end>
+              Cửa hàng
+            </NavLink>
+          </li>
+
+          <RenderIf condition={!!props.product}>
+            <li>
+              <NavLink to={`/products/${props.product?._id}`}>{props.product?.title}</NavLink>
+            </li>
+          </RenderIf>
+
+          <RenderIf condition={!!category}>
+            <li>
+              <NavLink
+                className={`after:text-gray-500 after:content-['_/_'] hover:text-red-500`}
+                to={`/products/?category=${category}`}
+              >
+                Phân loại: {category?.toUpperCase()}
+              </NavLink>
+            </li>
+          </RenderIf>
+
+          <RenderIf condition={!!query}>
+            <li>
+              <NavLink
+                className="after:text-gray-500 after:content-['_/_'] hover:text-red-500"
+                to={`/products/?${category ? "category=" + category : ""}&query=${query}`}
+                end
+              >
+                Tìm kiếm: {query}
+              </NavLink>
+            </li>
+          </RenderIf>
+        </RenderIf>
+
+        <RenderIf condition={/\/about-us/.test(location.pathname)}>
+          <li>
+            <NavLink to="/about-us">Hệ thống cửa hàng</NavLink>
+          </li>
+        </RenderIf>
+
+        <RenderIf condition={/\/contact-us/.test(location.pathname)}>
+          <li>
+            <NavLink to="/contact-us">Liên hệ</NavLink>
+          </li>
+        </RenderIf>
+      </ol>
+    </nav>
+  );
+};
+
+export default memo(Breadcrumbs);
